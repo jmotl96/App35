dab08e6be9c113af15f2a16ce875ead747c72af2
jmotl96 <jmotl96@gmail.com>
2020-04-29 13:38:03 -0500

    Initial commit

---
+++ App3/App3/DndClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace App3
+{
+    public class DndClass
+    {
+        public DndClass()
+        {
+            
+        }
+
+        public DndClass(String name, int points)
+        {
+            Name = name;
+            Points = points;
+        }
+        
+        public String Name { private set; get; }
+        
+        public int Points { set; get; }
+
+        static DndClass()
+        {
+            List<DndClass> all = new List<DndClass>
+                        {
+                            new DndClass("Barbarian",0),
+                            new DndClass("Bard",0),
+                            new DndClass("Cleric",0),
+                            new DndClass("Druid",0),
+                            new DndClass("Fighter",0),
+                            new DndClass("Monk",0),
+                            new DndClass("Rogue",0),
+                            new DndClass("Sorcerer", 0),
+                            new DndClass("Warlock",0),
+                            new DndClass("Wizard",0)
+                        };
+            All = all;
+        }
+        public static IList<DndClass> All { set; get; }
+
+
+    }
+}
---
+++ App3/App3/Questions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace App3
+{
+    public class Questions
+    {
+        private Questions(string whatWhouldYouLikeToUseInABattle)
+        {
+            
+        }
+
+        public Questions(String prompt, String left, String right, int affirmtive, int negative)
+        {
+            Prompt = prompt;
+            Left = left;
+            Right = right;
+            Affirmative = affirmtive;
+            Negative = negative;
+        }
+
+        public String Prompt { private set; get; }
+        public String Left { private set; get; }
+        public String Right { private set; get; }
+        public int Affirmative { private set; get; }
+        
+        public int Negative {private set; get;}
+
+        static Questions()
+        {
+            List<Questions> all = new List<Questions>
+            {
+                new Questions("What whould you like to use in a battle?", "Wepens made of steel","Magic", 0,4),
+                new Questions("What is your best atubute", "your stranth", "your charm",0,4)
+                
+            };
+            All = all;
+
+        }
+        
+        public static IList<Questions> All { private set; get; }
+    }
+}
